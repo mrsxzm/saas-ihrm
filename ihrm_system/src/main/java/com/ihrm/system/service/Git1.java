b7ea33fcd2f657d946d6b1f7b772fe9d07da66ae
mrsxzm <892484083@email.com>
2021-04-12 20:16:54 +0800

    创建git文件

---
+++ ihrm_system/src/main/java/com/ihrm/system/service/Git1.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C), 2019-2021, 微元素公司
+ * FileName: Git1
+ * Author:   xzm
+ * Date:     2021/4/12 20:14
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+package com.ihrm.system.service;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author xzm
+ * @create 2021/4/12
+ * @since 1.0.0
+ */
+public class Git1 {
+    public static void main(String[] args) {
+        System.out.println("555");
+    }
+}
