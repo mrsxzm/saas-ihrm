70833cdb6c4f8b779082a9436e38ce9eb0f798eb
mrsxzm <892484083@email.com>
2021-04-25 11:22:23 +0800

    改成5556

--- ihrm_system/src/main/java/com/ihrm/system/service/Git1.java
+++ ihrm_system/src/main/java/com/ihrm/system/service/Git1.java
@@ -21,6 +21,6 @@ package com.ihrm.system.service;
  */
 public class Git1 {
     public static void main(String[] args) {
-        System.out.println("555");
+        System.out.println("555222");
     }
 }
